85daf9cca0d3d9d052b696199d6632d6701fecd7
hanshihao <2737378492@qq.com>
2016-09-25 17:31:20 +0800

    Add files via upload

---
+++ move my name.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep 24 17:21:44 2016
+
+@author: 2014301020016
+"""
+for a in range(50):
+  import os
+  i = os.system('cls')
+  print a*" ","  ##### #     #    #                     "
+  print a*" "," ##     #          #                     "
+  print a*" ","  ####  ##### #    #####  ###    ###     "
+  print a*" ","     ## #   # #    #   # #   #  #   #    "
+  print a*" "," #####  #   # #    #   #  #####  ###     "
+  import time
+  time.sleep(0.05)
